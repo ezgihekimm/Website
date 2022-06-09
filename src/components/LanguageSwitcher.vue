8b04a2f42e99482817676ffb88cb8527bd8da75d
Kagan Inan <contact@kaaninan.net>
2022-06-09 20:02:31 +0300

    i18n path added

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="locale-changer">
-    <select v-model="$i18n.locale">
-      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
-    </select>
-  </div>
+	<div class="locale-changer">
+	<ul>
+		<li v-for="locale in langs" :key="locale" @click="switchLocale(locale)">
+			{{locale}}
+		</li>
+	</ul>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'locale-changer',
-  data () {
-    return { langs: ['en', 'tr'] }
-  }
+	name: 'locale-changer',
+	data () {
+		return { langs: ['en', 'tr'] }
+	},
+	methods: {
+		switchLocale(locale) {
+			if (this.$i18n.locale !== locale) {
+				this.$i18n.locale = locale;
+				const to = this.$router.resolve({ params: {locale} })
+				this.$router.push({path: to.path})
+			}
+		}
+	},
 }
 </script>
