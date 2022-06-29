40eeb519676daaa80da669c7ba627acdcb343e2b
ezgihekimm <84326561+ezgihekimm@users.noreply.github.com>
2022-06-29 14:01:47 +0300

    slider anim

---
+++ src/components/Patent2.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="_container">
+ff
+</div>
+    
+</template>
+
+
+<script>
+
+</script>
+
+
+<style scoped>
+._container{
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(89, 64, 64);
+    position: relative;
+  
+}
+
+</style>
